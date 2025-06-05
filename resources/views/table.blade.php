3f044a1d0d58b136c8a849d03f570a7b20ea4807
nuyulll14 <nuruljogja07@gmail.com>
2025-06-05 15:51:23 +0700

    acara12

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -2,31 +2,120 @@
 
 @section('content')
 <div class="container-mt-4">
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Image</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($points as $p)
-            <tr>
-                <td>{{ $p->id }}</td>
-                <td>{{ $p->name }}</td>
-                <td>{{ $p->description }}</td>
-                <td>
-                    <img src="{{ asset('storage/images/' . $p->image) }}" alt="" width="200" title="{{ $p->image }}">
-                </td>
-                <td>{{ $p->created_at }}</td>
-                <td>{{ $p->updated_at }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="card">
+        <div class="card-header">
+            <h4>Data Points</h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped" id="pointsTable">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($points as $p)
+                    <tr>
+                        <td>{{ $p->id }}</td>
+                        <td>{{ $p->name }}</td>
+                        <td>{{ $p->description }}</td>
+                        <td>
+                            <img src="{{ asset('storage/images/' . $p->image) }}" alt="" width="200" title="{{ $p->image }}">
+                        </td>
+                        <td>{{ $p->created_at }}</td>
+                        <td>{{ $p->updated_at }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="card mt-4">
+        <div class="card-header">
+            <h4>Data Polylines</h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped" id="polylinesTable">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($polyline as $p)
+                    <tr>
+                        <td>{{ $p->id }}</td>
+                        <td>{{ $p->name }}</td>
+                        <td>{{ $p->description }}</td>
+                        <td>
+                            <img src="{{ asset('storage/images/' . $p->image) }}" alt="" width="200" title="{{ $p->image }}">
+                        </td>
+                        <td>{{ $p->created_at }}</td>
+                        <td>{{ $p->updated_at }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="card mt-4">
+        <div class="card-header">
+            <h4>Polygons</h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped" id="polygonsTable">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($polygons as $p)
+                    <tr>
+                        <td>{{ $p->id }}</td>
+                        <td>{{ $p->name }}</td>
+                        <td>{{ $p->description }}</td>
+                        <td>
+                            <img src="{{ asset('storage/images/' . $p->image) }}" alt="" width="200" title="{{ $p->image }}">
+                        </td>
+                        <td>{{ $p->created_at }}</td>
+                        <td>{{ $p->updated_at }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+@endsection
+
+@section('styles')
+<link rel="stylesheet" href="https://cdn.datatables.net/2.3.1/css/dataTables.dataTables.min.css">
+@endsection
+
+@section('scripts')
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/2.3.1/js/dataTables.min.js"></script>
+<script>
+    let tablepoints = new DataTable('#pointsTable');
+    let tablepolylines= new DataTable('#polylinesTable');
+    let tablepolygons = new DataTable('#polygonsTable');
+</script>
 @endsection
